3d7fc4fc788fb979b4edacd40d4ee9b83a28fb18
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-24 13:06:43 +0300

    Updated src/settings by adding config('Debug')

--- src/django101/settings.py
+++ src/django101/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
