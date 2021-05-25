2a8065299f8d63b17a8b8559c657139a6ada1c74
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-25 11:42:29 +0300

    Modified blog/views.py due creating the blog views funcs

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import BlogPosts
+
+
+def blog_home_page(request):
+    obj = BlogPosts.objects.all()
+    context = {"obj": obj}
+    template_name = "blog_home_page.html"
+    return render(request, template_name, context)
+
+
+def blog_post_detail_page(request, pk):
+    bp_obj = BlogPosts.objects.get(id=pk)
+    template_name = "blog_post_detail.html"
+    context = {"bp_obj": bp_obj}
+    return render(request, template_name, context)
