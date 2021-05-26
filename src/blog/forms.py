64305b031beb36c9cea57e6034dfb54bb0eb880e
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-26 19:39:06 +0300

    Added validation in BlogPostModelForm

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -5,7 +5,7 @@ from .models import BlogPosts
 
 
 class BlogPostForm(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(max_length=60)
     slug = forms.SlugField()
     content = forms.CharField(widget=forms.Textarea)
     
@@ -18,3 +18,29 @@ class BlogPostModelForm(forms.ModelForm):
             'slug',
             'content',
         ]
+    
+    # This validation is equal if we set unique=True in .models BlogPost
+    def clean_title(self, *args, **kwargs):
+        """This validation function is checking 
+        if the title has been used in previous post,
+        and if raises an ValidationError.
+
+        Raises:
+            forms.ValidationError: [It can't be used same title for different posts]
+
+        Returns:
+            [title]: [This is the form object with title equal to the given title]
+        """
+        print(self.cleaned_data)
+        # Customer's given title.
+        title = self.cleaned_data.get('title')
+        
+        # Checks if the customer's title has been used.
+        qs = BlogPosts.objects.filter(title=title)
+        
+        # Raises error if the customer's title exist in db else it returns it.
+        if qs.exists():
+            raise forms.ValidationError(
+                'This title has already been used. Please enter new title!'
+                )
+        return title
