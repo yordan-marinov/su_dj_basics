4b1f5bea7d7b51713be267124a6f74d775561221
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-26 10:01:19 +0300

    Updated view with the slug field

--- src/blog/views.py
+++ src/blog/views.py
@@ -11,8 +11,8 @@ def blog_home_page(request):
     return render(request, template_name, context)
 
 
-def blog_all_posts_page(request, pk):
-    bp_obj = get_object_or_404(BlogPosts, id=pk)
+def blog_all_posts_page(request, slug):
+    bp_obj = get_object_or_404(BlogPosts, slug=slug)
     template_name = "blog_all_posts.html"
     context = {"bp_obj": bp_obj}
     return render(request, template_name, context)
