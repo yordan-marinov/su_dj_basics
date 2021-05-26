1474fbfef962b206655bab76af6ab2943cac14e5
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-26 11:32:36 +0300

    Added all the CRUD views to the blog/urls

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from .views import blog_home_page, blog_all_posts_page
+from .views import (
+    blog_post_list_view,
+    blog_post_create_view,
+    blog_post_retrieve_view,
+    blog_post_update_view,
+    blog_post_delete_view,
+)
 
 
 urlpatterns = [
-    path("", blog_home_page),
-    path("<str:slug>/", blog_all_posts_page),
-    # path('<int:pk>/', blog_all_posts_page),
-]
+    path("", blog_post_list_view),
+    path("create/", blog_post_create_view),
+    path('<str:slug>/', blog_post_retrieve_view),
+    path('<str:slug>/edit/', blog_post_update_view),
+    path('<str:slug>/delete', blog_post_delete_view),
+    ]
