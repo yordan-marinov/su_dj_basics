4fb51e95fd42e6790c390fe4cb006ba730201d84
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-26 09:56:02 +0300

    Added slug field to the db model

--- src/blog/models.py
+++ src/blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class BlogPosts(models.Model):
     title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
 
     def __str__(self) -> str:
