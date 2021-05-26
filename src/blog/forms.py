002425c2d947f1e8891acc43fbc94f38e84e4fd8
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-26 16:09:40 +0300

    Updated blog/forms by adding BlogPostModelForm

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.forms import fields
+
+from .models import BlogPosts
 
 
 class BlogPostForm(forms.Form):
     title = forms.CharField()
     slug = forms.SlugField()
     content = forms.CharField(widget=forms.Textarea)
+    
+    
+class BlogPostModelForm(forms.ModelForm):
+    class Meta:
+        model = BlogPosts
+        fields = [
+            'title',
+            'slug',
+            'content',
+        ]
