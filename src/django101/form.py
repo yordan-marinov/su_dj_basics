952e6a26acde0492a227d03228d718c21c23ccb3
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-26 18:47:58 +0300

    Added a validation func into a django/form

--- src/django101/form.py
+++ src/django101/form.py
@@ -5,4 +5,14 @@ class ContactForm(forms.Form):
     full_name = forms.CharField(max_length=50)
     email = forms.EmailField()
     content = forms.CharField(widget=forms.Textarea)
-    
+
+    def clean_email(self, *args, **kwargs):
+        """This is a validation func which validates the email if they ends with .bg extension
+        and if they do it raises ValidatonError to the client."""
+
+        email = self.cleaned_data.get("email") # => email=form.cleaned_data.get('email')
+        # In this case self refers to the form object.
+        print(f"== email ==> {email}")
+        if email.endswith(".bg"):
+            raise forms.ValidationError("This is not a valid email!")
+        return email
