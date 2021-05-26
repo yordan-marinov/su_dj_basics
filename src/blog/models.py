db135de7a2850e63f8f2b8c3e8e7190ef0fb63c2
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-26 19:40:24 +0300

    Set the unique=True into blog/models

--- src/blog/models.py
+++ src/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class BlogPosts(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
 
