71af26f8e43177f32d6a27891c3697c4fba02c71
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-26 12:17:30 +0300

    Updated blog/urls

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
     blog_post_list_view,
-    blog_post_create_view,
+    # blog_post_create_view,
     blog_post_retrieve_view,
     blog_post_update_view,
     blog_post_delete_view,
@@ -11,7 +11,7 @@ from .views import (
 
 urlpatterns = [
     path("", blog_post_list_view),
-    path("create/", blog_post_create_view),
+    # path("create/", blog_post_create_view),
     path('<str:slug>/', blog_post_retrieve_view),
     path('<str:slug>/edit/', blog_post_update_view),
     path('<str:slug>/delete', blog_post_delete_view),
