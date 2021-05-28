7fa5fc08e9179ce3b000eb591f6ca64d17094de4
yordan-marinov <jordanmarinov8@gmail.com>
2021-05-28 17:16:56 +0300

    Added paths for the update view and also the delete one

--- src/blog_2/urls.py
+++ src/blog_2/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from .views import(
     list_page,
-    create_page,
     details_page,
     update_page,
-    delet_page,
+    delete_page,
 )
 
 urlpatterns = [
     path("", list_page, name='list'),
-    path('<str:slug>/', details_page, name='details')
+    path('<str:slug>/', details_page, name='details'),
+    path('<str:slug>/edit/', update_page, name="update"),
+    path('<str:slug>/delete/', delete_page, name='delete'),
     ]
