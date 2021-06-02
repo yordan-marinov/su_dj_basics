35289d676e11087e50d848e074c432a42b6b19f7
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-02 12:09:23 +0300

    Added Product modiel

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=240)
+    content = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+        
