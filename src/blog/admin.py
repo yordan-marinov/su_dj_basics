0ad6b159d93b0f62ce4eac227ed9c8a087200d18
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-07 09:43:13 +0300

    Register Author to the blog/admin

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import BlogPosts
+from .models import BlogPosts, Author
 
 
 admin.site.register(BlogPosts)
+admin.site.register(Author)
