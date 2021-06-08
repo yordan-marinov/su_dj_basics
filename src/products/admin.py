46fb7e5c859b02d04d296694f21a55a07c0fc12c
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-08 10:16:53 +0300

    Registered the added models to the admin module

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import (
+    Product,
+    Color,
+    CarMake,
+    CarModel,
+    Owner,
+    Residence,
+)
 
 
 admin.site.register(Product)
+admin.site.register(Color)
+admin.site.register(CarMake)
+admin.site.register(CarModel)
+admin.site.register(Owner)
+admin.site.register(Residence)
