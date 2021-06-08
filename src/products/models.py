ea9e935d96624b168784f00565f70cbf6b09d2a0
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-08 10:16:09 +0300

    Added different models to the models.py

--- src/products/models.py
+++ src/products/models.py
@@ -9,3 +9,55 @@ class Product(models.Model):
     def __str__(self):
         return self.title
         
+        
+
+class Color(models.Model):
+    name = models.CharField(max_length=50)
+    car_makes = models.ManyToManyField("CarMake")
+
+    def __str__(self):
+        return self.name
+
+
+class CarMake(models.Model):
+    CHOICES = (
+        ("Estate", "estate"),
+        ("Coupe", "coupe"),
+        ("Sedan", "sedan"),
+        ("Hachback", "hachback"),
+    )
+
+    name = models.CharField(max_length=50)
+    car_type = models.CharField(max_length=50, choices=CHOICES)
+    owner = models.ForeignKey("Owner", on_delete=models.CASCADE)
+    model = models.OneToOneField(
+        "CarModel",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class CarModel(models.Model):
+    model = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.model
+
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    residence = models.ForeignKey("Residence", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Residence(models.Model):
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.city}, {self.country}"
