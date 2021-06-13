0918ff8400748f9de8f007388940ad272e23b873
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-13 16:18:59 +0300

    Created the Task model in todo app

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        (3, "High"),
+        (2, "Medium"),
+        (1, "Low"),
+    )
+
+    task = models.CharField(max_length=120)
+    description = models.TextField()
+    complete = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES)
+
+    class Meta:
+        ordering = ["status", 'priority']
+
+    def __str__(self):
+        return self.task
