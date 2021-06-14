b58d0359a53b50c5f8660bbc31afd33e0090d992
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-14 20:30:58 +0300

    Added create and delete urls in todo/urls

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import todo_list
+from .views import todo_create, todo_edit, todo_list
 
 urlpatterns = [
     path('', todo_list, name='todo_list'),
+    path('create/', todo_create, name='todo_create'),
+    path('edit/<int:pk>', todo_edit, name='todo_edit'),
 ]
