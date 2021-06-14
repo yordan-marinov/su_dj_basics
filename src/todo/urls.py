9446c4a49c11b6f411af98f62b66535310cb095e
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-14 21:15:27 +0300

    Modified the todo/ulrs due to added delete url

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import todo_create, todo_edit, todo_list
+from .views import todo_create, todo_delete, todo_edit, todo_list
 
 urlpatterns = [
-    path('', todo_list, name='todo_list'),
-    path('create/', todo_create, name='todo_create'),
-    path('edit/<int:pk>', todo_edit, name='todo_edit'),
+    path("", todo_list, name="todo_list"),
+    path("create/", todo_create, name="todo_create"),
+    path("edit/<int:pk>/", todo_edit, name="todo_edit"),
+    path("delete/<int:pk>/", todo_delete, name="todo_delete"),
 ]
