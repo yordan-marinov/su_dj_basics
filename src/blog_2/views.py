1cde85122bd94b8649c7c8c966b391dda4ce613f
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-17 12:03:59 +0300

    Added button for details, update, delete in list.html

--- src/blog_2/views.py
+++ src/blog_2/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render, get_object_or_404
 
 from .models import Post
 from .forms import PostForm, PostModelForm
@@ -13,61 +12,30 @@ from .forms import PostForm, PostModelForm
 
 
 def list_page(request):
-    """This is a view function wich takes request object
-    and render html file dynamicaly by giving db objects.
-    It list out all blog posts.
-
-    Args:
-        request : Client request object passed by django
-
-    Returns:
-        html: Dynamicly generated view of the html template
-    """
     qs = Post.objects.all()
     template_name = "blog_2/list.html"
-    context = {"page_name": "list_page", "obj": qs}
+    context = {"page_name": "Posts", "posts": qs}
+
     return render(request, template_name, context)
 
-    # This Create Page View is using the form.Form
-    # def create_page(request):
+    # ===  Create Page View is using the form.Form ==========================
     form = PostForm(request.POST or None)
     if form.is_valid():
-
-        # This is simple print debug
-        # Only to get my head around of form instance
-        # And there is no need to do it at all
-        print("===> It works!")
-        print(f"===> {form.__dict__}")
-        print(f"===> Form: {form.cleaned_data}")
-
+        # print("===> It works!")
+        # print(f"===> {form.__dict__}")
+        # print(f"===> Form: {form.cleaned_data}")
         # Creating a Model instance by create method of the Post object
         # And passing kwargs of the form.cleaned_data
         Post.objects.create(**form.cleaned_data)
-
         # Redirect to the empty post form after creating Post instance
-        return HttpResponseRedirect("/blog-2-new")
-
+        return redirect("create")
     template_name = "blog_2/create.html"
     context = {"form": form}
     return render(request, template_name, context)
-
-    # def create_page(request):
-    pass
+    # ======================================================================
 
 
-# This Create Page View is using the form.ModelForm
 def create_page(request):
-    """This is a func which renders ModelForm 
-    which needs to be filled in correctly by the user
-    and returns respose.
-
-    Args:
-        request : Client request to create a new post
-
-    Returns:
-        Render http response with the valid filled form
-    """
-    # assign postmodelform instance to the form var
     form = PostModelForm(request.POST or None)
 
     # Checks if the form is with valid input
@@ -75,7 +43,7 @@ def create_page(request):
         # Seves the data from the form to the Post model(db)
         form.save()
         # It redirects customer to the blog-2 list view
-        return HttpResponseRedirect("/blog-2")
+        return redirect("list")
 
     template_name = "blog_2/create.html"
     context = {"form": form}
@@ -98,40 +66,32 @@ def details_page(request, slug):
 
     # It returns Post object if slug is valid
     # otherwise raise 404 error exeption
-    qs = get_object_or_404(Post, slug=slug)
+    obj = get_object_or_404(Post, slug=slug)
     template_name = "blog_2/detais.html"
-    context = {"post_obj": qs}
+    context = {"page_name": "Details", "post_obj": obj}
 
     return render(request, template_name, context)
 
 
 def update_page(request, slug):
-    qs = get_object_or_404(Post, slug=slug)
-    form = PostModelForm(request.POST or None, instance=qs)
+    obj = get_object_or_404(Post, slug=slug)
+    form = PostModelForm(request.POST or None, instance=obj)
 
     if form.is_valid():
         form.save()
-        return HttpResponseRedirect("/blog-2")
+        return redirect("list")
 
     template_name = "blog_2/create.html"
-    context = {"form": form}
+    context = {"page_name": "Updates", "form": form}
 
     return render(request, template_name, context)
 
 
 def delete_page(request, slug):
-    """It Delete the object with the given slug 
-    if is not found raise 404 exeption
-    and returns redirected responce to the blog_2 list.
+    obj = get_object_or_404(Post, slug=slug)
+    if request.method == "POST":
+        obj.delete()
+        return redirect("list")
 
-    Args:
-        request: Customers request pass by Django
-        slug (str): Unique str identification field set by the author of the post
-
-    Returns:
-        Http: Redirected to the blog_2 list
-    """
-    
-    qs = get_object_or_404(Post, slug=slug)
-    qs.delete()
-    return HttpResponseRedirect("/blog-2")
+    context = {"page_name": "Delete", "post": obj}
+    return render(request, "blog_2/delete.html", context)
