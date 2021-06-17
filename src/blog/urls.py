d5d28ba3582963aa0d19928dd934c84b934e9757
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-17 10:00:27 +0300

    Modified blog/urls due to add path for author url

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,16 +2,18 @@ from django.urls import path
 
 from .views import (
     blog_post_list_view,
-    # blog_post_create_view,
+    blog_post_create_view,
     blog_post_retrieve_view,
     blog_post_update_view,
     blog_post_delete_view,
+    create_author,
 )
 
 
 urlpatterns = [
     path("", blog_post_list_view, name='list_name'),
-    # path("create/", blog_post_create_view),
+    path('author/', create_author, name="create_author"),
+    path("create/", blog_post_create_view, name='create_name'),
     path('<str:slug>/', blog_post_retrieve_view, name='details_name'),
     path('<str:slug>/edit/', blog_post_update_view, name='edit_name'),
     path('<str:slug>/delete', blog_post_delete_view, name='delete_name'),
