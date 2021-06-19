e3ee8fa7de512ff21e1a1d6f2fbd0ec5d5ec1c7e
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-19 17:39:56 +0300

    Added agent details url

--- src/crm/urls.py
+++ src/crm/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from crm.views import crm_lead_list
+from crm.views import crm_lead_list, details_agent, details_lead
 
 
 urlpatterns = [
-    path('', crm_lead_list, name='crm_lead_list')
+    path('', crm_lead_list, name='crm_lead_list'),
+    path('<int:pk>', details_lead, name='crm_lead_details'),
+    path('agent/<int:pk>', details_agent, name='crm_agent_details'),
 ]
