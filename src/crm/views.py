fd00b30dea5ee80f83a70adf784fa0da80b3d6bc
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-19 19:03:45 +0300

    Added the create view with the lead Model form

--- src/crm/views.py
+++ src/crm/views.py
@@ -1,50 +1,60 @@
 from typing import ContextManager
 from django.shortcuts import redirect, render
 
-from crm.forms import LeadForm
+from crm.forms import LeadForm, LeadModelForm
 
 from .models import Agent, Lead
 
 
 def crm_lead_list(request):
     qs = Lead.objects.all()
-    context = {
-        'leads': qs, 
-        'page_name': 'leads'
-        }
+    context = {"leads": qs, "page_name": "leads"}
     return render(request, "crm/crm_lead_list.html", context)
 
 
 def details_lead(request, pk):
     obj = Lead.objects.get(pk=pk)
-    context = {'lead': obj, 'page_name': 'leads'}
-    return render(request, 'crm/crm_lead_details.html', context)
-    
+    context = {"lead": obj, "page_name": "leads"}
+    return render(request, "crm/crm_lead_details.html", context)
+
 
 def details_agent(request, pk):
     obj = Agent.objects.get(pk=pk)
     print(obj)
-    context = {'agent': obj}
-    return render(request, 'crm/crm_agent_details.html', context)
+    context = {"agent": obj}
+    return render(request, "crm/crm_agent_details.html", context)
 
 
-def create_lead(request):
+def create_lead_form(request):
     form = LeadForm(request.POST or None)
     if form.is_valid():
         agent = Agent.objects.first()
-        first_name = form.cleaned_data['first_name']
-        last_name = form.cleaned_data['last_name']
-        age = form.cleaned_data['age']
+        first_name = form.cleaned_data["first_name"]
+        last_name = form.cleaned_data["last_name"]
+        age = form.cleaned_data["age"]
         lead = Lead(
             first_name=first_name,
             last_name=last_name,
             age=age,
-            agent=agent, 
+            agent=agent,
         )
         lead.save()
-        return redirect('crm_lead_list')
-    
+        return redirect("crm_lead_list")
+
     context = {
-        'form': form,
+        "form": form,
     }
-    return render(request, 'crm/crm_create_lead.html', context)
+    return render(request, "crm/crm_create_lead.html", context)
+
+
+def create_lead(request):
+    form = LeadModelForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("crm_lead_list")
+
+    context = {
+        "form": form,
+        "page_name": "Create Page",
+    }
+    return render(request, "crm/crm_create_lead.html", context)
