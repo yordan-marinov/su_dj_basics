53e92479d436132b7d184c035f36140df52fe891
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-19 16:45:04 +0300

    Implemented the lead_list view

--- src/crm/views.py
+++ src/crm/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Lead
+
+
+def crm_lead_list(request):
+    qs = Lead.objects.all()
+    context = {
+        'leads': qs, 
+        'page_name': 'leads'
+        }
+    return render(request, "crm/crm_lead_list.html", context)
+
+
+def details_lead(request, pk):
+    pass
+    
