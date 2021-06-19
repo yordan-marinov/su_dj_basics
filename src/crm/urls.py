aeaa6b5fc63642b3d633c0c241ec208c154f03dc
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-19 20:08:49 +0300

    Added delete url

--- src/crm/urls.py
+++ src/crm/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from crm.views import create_lead, crm_lead_list, details_agent, details_lead
+from crm.views import create_lead, crm_lead_list, delete_lead, details_agent, details_lead
 
 
 urlpatterns = [
     path('', crm_lead_list, name='crm_lead_list'),
     path('<int:pk>', details_lead, name='crm_lead_details'),
     path('agent/<int:pk>', details_agent, name='crm_agent_details'),
-    path('create/', create_lead, name='crm_lead_create')
+    path('create/', create_lead, name='crm_lead_create'),
+    path('delete/<int:pk>', delete_lead, name='crm_lead_delete'),
 ]
