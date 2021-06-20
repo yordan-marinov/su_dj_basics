87824840b24496f717ce61a2f4a5fc446e617797
yordan-marinov <jordanmarinov8@gmail.com>
2021-06-20 15:42:46 +0300

    Added edit url

--- src/crm/urls.py
+++ src/crm/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from crm.views import create_lead, crm_lead_list, delete_lead, details_agent, details_lead
+from crm.views import create_lead, crm_lead_list, delete_lead, details_agent, details_lead, edit_lead
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('agent/<int:pk>', details_agent, name='crm_agent_details'),
     path('create/', create_lead, name='crm_lead_create'),
     path('delete/<int:pk>', delete_lead, name='crm_lead_delete'),
+    path('edit/<int:pk>', edit_lead, name='crm_lead_edit'),
 ]
